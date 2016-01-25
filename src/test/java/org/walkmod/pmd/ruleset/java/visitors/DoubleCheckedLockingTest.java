61c3d23fed64c2d57f58c27f0a5d90852a9dc50e
rpau <rpau@ac.upc.edu>
2016-01-25 09:25:33 +0100

    DoubleCheckedLocking

---
+++ src/test/java/org/walkmod/pmd/ruleset/java/visitors/DoubleCheckedLockingTest.java
@@ -0,0 +1,28 @@
+package org.walkmod.pmd.ruleset.java.visitors;
+
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.walkmod.javalang.ASTManager;
+import org.walkmod.javalang.ast.CompilationUnit;
+import org.walkmod.javalang.ast.body.MethodDeclaration;
+import org.walkmod.javalang.ast.stmt.BlockStmt;
+import org.walkmod.javalang.ast.stmt.IfStmt;
+import org.walkmod.javalang.ast.stmt.SynchronizedStmt;
+
+
+public class DoubleCheckedLockingTest {
+
+   @Test
+   public void testMissingEquals() throws Exception {
+      CompilationUnit cu = ASTManager.parse(new File("src/test/resources/examples/doubleCheckedLocking.txt"));
+      DoubleCheckedLocking<?> visitor = new DoubleCheckedLocking<Object>();
+      visitor.visit(cu, null);
+      MethodDeclaration md = (MethodDeclaration)cu.getTypes().get(0).getMembers().get(1);
+      BlockStmt block = md.getBody();
+      IfStmt ifStmt = (IfStmt)block.getStmts().get(0);
+      BlockStmt stmt = (BlockStmt)ifStmt.getThenStmt();
+      Assert.assertTrue(stmt.getStmts().get(0) instanceof SynchronizedStmt);
+   }
+}
