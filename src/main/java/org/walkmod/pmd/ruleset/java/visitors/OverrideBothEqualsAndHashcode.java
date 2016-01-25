e20f43f935f51775e0866f44beec65dcdfe63a34
rpau <rpau@ac.upc.edu>
2016-01-25 08:01:39 +0100

    OverrideBothEqualsAndHashcode

---
+++ src/main/java/org/walkmod/pmd/ruleset/java/visitors/OverrideBothEqualsAndHashcode.java
@@ -0,0 +1,85 @@
+package org.walkmod.pmd.ruleset.java.visitors;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.walkmod.javalang.ASTManager;
+import org.walkmod.javalang.ParseException;
+import org.walkmod.javalang.ast.body.BodyDeclaration;
+import org.walkmod.javalang.ast.body.ClassOrInterfaceDeclaration;
+import org.walkmod.javalang.ast.body.MethodDeclaration;
+import org.walkmod.javalang.ast.body.ModifierSet;
+import org.walkmod.javalang.ast.body.Parameter;
+import org.walkmod.javalang.ast.body.VariableDeclaratorId;
+import org.walkmod.javalang.ast.stmt.BlockStmt;
+import org.walkmod.javalang.ast.type.ClassOrInterfaceType;
+import org.walkmod.javalang.ast.type.Type;
+import org.walkmod.javalang.visitors.VoidVisitorAdapter;
+
+public class OverrideBothEqualsAndHashcode<T> extends VoidVisitorAdapter<T> {
+
+   public void visit(ClassOrInterfaceDeclaration coid, T ctx) {
+      if (!coid.isInterface()) {
+         List<BodyDeclaration> members = coid.getMembers();
+         boolean hasEquals = false;
+         boolean hasHashCode = false;
+         if (members != null) {
+            Iterator<BodyDeclaration> it = members.iterator();
+            while (it.hasNext() && (!hasEquals || !hasHashCode)) {
+               BodyDeclaration bd = it.next();
+               if (bd instanceof MethodDeclaration) {
+                  MethodDeclaration md = ((MethodDeclaration) bd);
+                  List<Parameter> params = md.getParameters();
+                  String name = md.getName();
+                  if (name.equals("equals") && params != null && params.size() == 1) {
+                     Parameter param = params.get(0);
+                     Type type = param.getType();
+                     String typeName = type.toString();
+                     if (typeName.equals("Object") || typeName.equals("java.lang.Object")) {
+                        hasEquals = true;
+                     }
+                  } else if (name.equals("hashCode") && (params == null || params.isEmpty())) {
+                     hasHashCode = true;
+                  }
+               } else {
+                  bd.accept(this, ctx);
+               }
+            }
+
+            if (hasEquals && !hasHashCode) {
+               MethodDeclaration md = new MethodDeclaration();
+               md.setName("hashCode");
+               md.setModifiers(ModifierSet.PUBLIC);
+               md.setType(new ClassOrInterfaceType("int"));
+               BlockStmt body = null;
+               try {
+                  body = (BlockStmt) ASTManager.parse(BlockStmt.class, "{ super.hashCode(); }");
+               } catch (ParseException e) {
+                  throw new RuntimeException("Error generating hashCode method");
+               }
+               md.setBody(body);
+               members.add(md);
+            } else if (!hasEquals && hasHashCode) {
+               MethodDeclaration md = new MethodDeclaration();
+               md.setName("equals");
+               md.setModifiers(ModifierSet.PUBLIC);
+               md.setType(new ClassOrInterfaceType("boolean"));
+               List<Parameter> params = new LinkedList<Parameter>();
+               Parameter param = new Parameter(new ClassOrInterfaceType("Object"), new VariableDeclaratorId("o"));
+               params.add(param);
+               md.setParameters(params);
+               BlockStmt body = null;
+               try {
+                  body = (BlockStmt) ASTManager.parse(BlockStmt.class, "{ super.equals(o); }");
+               } catch (ParseException e) {
+                  throw new RuntimeException("Error generating equals method");
+               }
+               md.setBody(body);
+               members.add(md);
+            }
+
+         }
+      }
+   }
+}
---
+++ src/test/java/org/walkmod/pmd/ruleset/java/visitors/OverrideBothEqualsAndHashcodeTest.java
@@ -0,0 +1,69 @@
+package org.walkmod.pmd.ruleset.java.visitors;
+
+import java.io.File;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.walkmod.javalang.ASTManager;
+import org.walkmod.javalang.ast.CompilationUnit;
+import org.walkmod.javalang.ast.body.BodyDeclaration;
+import org.walkmod.javalang.ast.body.MethodDeclaration;
+
+public class OverrideBothEqualsAndHashcodeTest {
+
+   @Test
+   public void testMissingEquals() throws Exception {
+      CompilationUnit cu = ASTManager.parse(new File("src/test/resources/examples/overrideBothEqualsAndHashcode.txt"));
+      List<BodyDeclaration> members = cu.getTypes().get(0).getMembers();
+      Assert.assertNotNull(members);
+      Assert.assertEquals(1, members.size());
+      OverrideBothEqualsAndHashcode<?> visitor = new OverrideBothEqualsAndHashcode<Object>();
+      visitor.visit(cu, null);
+      members = cu.getTypes().get(0).getMembers();
+      Assert.assertNotNull(members);
+      Assert.assertEquals(2, members.size());
+      Assert.assertEquals("equals", ((MethodDeclaration) members.get(1)).getName());
+   }
+
+   @Test
+   public void testMissingHashCode() throws Exception {
+      CompilationUnit cu = ASTManager.parse(new File("src/test/resources/examples/overrideBothEqualsAndHashcode2.txt"));
+      List<BodyDeclaration> members = cu.getTypes().get(0).getMembers();
+      Assert.assertNotNull(members);
+      Assert.assertEquals(1, members.size());
+      OverrideBothEqualsAndHashcode<?> visitor = new OverrideBothEqualsAndHashcode<Object>();
+      visitor.visit(cu, null);
+      members = cu.getTypes().get(0).getMembers();
+      Assert.assertNotNull(members);
+      Assert.assertEquals(2, members.size());
+      Assert.assertEquals("hashCode", ((MethodDeclaration) members.get(1)).getName());
+   }
+
+   @Test
+   public void testBothAppear() throws Exception {
+      CompilationUnit cu = ASTManager.parse(new File("src/test/resources/examples/overrideBothEqualsAndHashcode3.txt"));
+      List<BodyDeclaration> members = cu.getTypes().get(0).getMembers();
+      Assert.assertNotNull(members);
+      Assert.assertEquals(2, members.size());
+      OverrideBothEqualsAndHashcode<?> visitor = new OverrideBothEqualsAndHashcode<Object>();
+      visitor.visit(cu, null);
+      members = cu.getTypes().get(0).getMembers();
+      Assert.assertNotNull(members);
+      Assert.assertEquals(2, members.size());
+
+   }
+   
+   @Test
+   public void testNeitherAppear() throws Exception{
+      CompilationUnit cu = ASTManager.parse(new File("src/test/resources/examples/overrideBothEqualsAndHashcode4.txt"));
+      List<BodyDeclaration> members = cu.getTypes().get(0).getMembers();
+      Assert.assertNotNull(members);
+      Assert.assertEquals(2, members.size());
+      OverrideBothEqualsAndHashcode<?> visitor = new OverrideBothEqualsAndHashcode<Object>();
+      visitor.visit(cu, null);
+      members = cu.getTypes().get(0).getMembers();
+      Assert.assertNotNull(members);
+      Assert.assertEquals(2, members.size());
+   }
+}
