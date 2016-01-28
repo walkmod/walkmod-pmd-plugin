eecce010e469d94d02d19afa8ed4bbc5434070be
rpau <rpau@ac.upc.edu>
2016-01-28 13:14:52 +0100

    MisplacedNullCheck

---
+++ src/main/java/org/walkmod/pmd/ruleset/java/basic/visitors/MisplacedNullCheck.java
@@ -0,0 +1,148 @@
+package org.walkmod.pmd.ruleset.java.basic.visitors;
+
+import org.walkmod.javalang.ast.FieldSymbolData;
+import org.walkmod.javalang.ast.Node;
+import org.walkmod.javalang.ast.SymbolDataAware;
+import org.walkmod.javalang.ast.SymbolDefinition;
+import org.walkmod.javalang.ast.SymbolReference;
+import org.walkmod.javalang.ast.expr.BinaryExpr;
+import org.walkmod.javalang.ast.expr.BinaryExpr.Operator;
+import org.walkmod.javalang.ast.expr.Expression;
+import org.walkmod.javalang.ast.expr.FieldAccessExpr;
+import org.walkmod.javalang.ast.expr.NameExpr;
+import org.walkmod.javalang.ast.expr.NullLiteralExpr;
+import org.walkmod.javalang.compiler.symbols.RequiresSemanticAnalysis;
+import org.walkmod.javalang.visitors.VoidVisitorAdapter;
+import org.walkmod.pmd.visitors.AbstractPMDRuleVisitor;
+
+@RequiresSemanticAnalysis
+public class MisplacedNullCheck<T> extends AbstractPMDRuleVisitor<T> {
+
+   @Override
+   public void visit(BinaryExpr n, T ctx) {
+      Operator op = n.getOperator();
+      if (op.equals(Operator.notEquals) || op.equals(Operator.equals)) {
+         Expression right = n.getRight();
+         Expression left = n.getLeft();
+         Node parent = n.getParentNode();
+
+         if (parent instanceof BinaryExpr) {
+            BinaryExpr bo = (BinaryExpr) parent;
+            if (right instanceof NullLiteralExpr) {
+
+               if (left instanceof NameExpr) {
+
+                  if (bo.getRight() == n) {
+                     ReorderCondition visitor = new ReorderCondition((NameExpr) left);
+                     bo.getLeft().accept(visitor, bo);
+                  }
+
+               } else if (left instanceof FieldAccessExpr) {
+                  if (bo.getRight() == n) {
+                     ReorderCondition visitor = new ReorderCondition((FieldAccessExpr) left);
+                     bo.getLeft().accept(visitor, bo);
+                  }
+               }
+            } else if (left instanceof NullLiteralExpr) {
+               
+               if (right instanceof NameExpr) {
+
+                  if (bo.getRight() == n) {
+                     ReorderCondition visitor = new ReorderCondition((NameExpr) right);
+                     bo.getLeft().accept(visitor, bo);
+                  }
+
+               } else if (right instanceof FieldAccessExpr) {
+                  if (bo.getRight() == n) {
+                     ReorderCondition visitor = new ReorderCondition((FieldAccessExpr) right);
+                     bo.getLeft().accept(visitor, bo);
+                  }
+               }
+            }
+         }
+      } else {
+         super.visit(n, ctx);
+      }
+   }
+
+   private class ReorderCondition extends VoidVisitorAdapter<BinaryExpr> {
+
+      private SymbolReference referredExpression;
+
+      public ReorderCondition(NameExpr referredExpression) {
+         this.referredExpression = referredExpression;
+      }
+
+      public ReorderCondition(FieldAccessExpr referredExpression) {
+         this.referredExpression = referredExpression;
+      }
+
+      @Override
+      public void visit(NameExpr n, BinaryExpr ctx) {
+
+         replace(n, ctx);
+      }
+
+      private void replace(SymbolReference n, BinaryExpr ctx) {
+
+         SymbolDefinition sd = n.getSymbolDefinition();
+
+         boolean valid = false;
+
+         if (sd != null && sd == referredExpression.getSymbolDefinition()) {
+            SymbolReference srParent = null;
+            SymbolReference srParentRef = null;
+            Node parentNode = null;
+
+            if (n instanceof FieldAccessExpr) {
+               parentNode = ((FieldAccessExpr) n).getScope();
+               if (parentNode != null) {
+                  if (parentNode instanceof SymbolReference) {
+                     srParent = (SymbolReference) parentNode;
+                  }
+               }
+            }
+
+            if (referredExpression instanceof FieldAccessExpr) {
+               parentNode = ((FieldAccessExpr) referredExpression).getScope();
+               if (parentNode != null) {
+                  if (parentNode instanceof SymbolReference) {
+                     srParentRef = (SymbolReference) parentNode;
+                  }
+               }
+            }
+
+            valid = (srParent == null && srParentRef == null) || (srParent != null && srParentRef != null
+                  && srParent.getSymbolDefinition() == srParentRef.getSymbolDefinition());
+
+         }
+         if (sd == null && referredExpression.getSymbolDefinition() == null) {
+            if (n instanceof SymbolDataAware && referredExpression instanceof SymbolDataAware) {
+               SymbolDataAware<?> sda = (SymbolDataAware<?>) n;
+               SymbolDataAware<?> sda2 = (SymbolDataAware<?>) referredExpression;
+               if (sda.getSymbolData() instanceof FieldSymbolData && sda2.getSymbolData() instanceof FieldSymbolData) {
+                  FieldSymbolData fsd = (FieldSymbolData) sda.getSymbolData();
+                  FieldSymbolData fsd2 = (FieldSymbolData) sda2.getSymbolData();
+                  valid = (fsd.getField().equals(fsd2.getField()));
+               }
+            }
+         }
+
+         if (valid) {
+
+            Expression left = ctx.getLeft();
+            Expression right = ctx.getRight();
+            ctx.setRight(left);
+            ctx.setLeft(right);
+
+         }
+      }
+
+      @Override
+      public void visit(FieldAccessExpr n, BinaryExpr ctx) {
+
+         replace(n, ctx);
+
+      }
+   }
+}
---
+++ src/test/java/org/walkmod/pmd/ruleset/java/basic/visitors/MisplacedNullCheckTest.java
@@ -0,0 +1,175 @@
+package org.walkmod.pmd.ruleset.java.basic.visitors;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.walkmod.javalang.ast.CompilationUnit;
+import org.walkmod.javalang.ast.body.MethodDeclaration;
+import org.walkmod.javalang.ast.expr.BinaryExpr;
+import org.walkmod.javalang.ast.expr.MethodCallExpr;
+import org.walkmod.javalang.ast.stmt.BlockStmt;
+import org.walkmod.javalang.ast.stmt.IfStmt;
+import org.walkmod.javalang.test.SemanticTest;
+
+public class MisplacedNullCheckTest extends SemanticTest {
+
+   @Test
+   public void testAnd() throws Exception {
+
+      CompilationUnit cu = compile(
+            "public class Foo{ public void bar(Object c){ if(c.toString().equals(\"hello\") && c!= null) {}} }");
+
+      MisplacedNullCheck<?> mnc = new MisplacedNullCheck<Object>();
+
+      mnc.visit(cu, null);
+
+      MethodDeclaration md = (MethodDeclaration) cu.getTypes().get(0).getMembers().get(0);
+
+      BlockStmt block = md.getBody();
+
+      IfStmt stmt = (IfStmt) block.getStmts().get(0);
+
+      BinaryExpr be = (BinaryExpr) stmt.getCondition();
+
+      BinaryExpr lbe = (BinaryExpr) be.getLeft();
+
+      Assert.assertTrue(lbe.getOperator().equals(BinaryExpr.Operator.notEquals));
+   }
+
+   @Test
+   public void testReversedNullChecking() throws Exception {
+
+      CompilationUnit cu = compile(
+            "public class Foo{ public void bar(Object c){ if(c.toString().equals(\"hello\") && null != c) {}} }");
+
+      MisplacedNullCheck<?> mnc = new MisplacedNullCheck<Object>();
+
+      mnc.visit(cu, null);
+
+      MethodDeclaration md = (MethodDeclaration) cu.getTypes().get(0).getMembers().get(0);
+
+      BlockStmt block = md.getBody();
+
+      IfStmt stmt = (IfStmt) block.getStmts().get(0);
+
+      BinaryExpr be = (BinaryExpr) stmt.getCondition();
+
+      BinaryExpr lbe = (BinaryExpr) be.getLeft();
+
+      Assert.assertTrue(lbe.getOperator().equals(BinaryExpr.Operator.notEquals));
+   }
+
+   @Test
+   public void testAndWithFieldAccess() throws Exception {
+
+      CompilationUnit cu = compile(
+            "public class Foo{ public void bar(){ if(Bar.name.equals(\"hello\") && Bar.name != null) {}} }",
+            "public class Bar{ public static Object name; }");
+
+      MisplacedNullCheck<?> mnc = new MisplacedNullCheck<Object>();
+
+      mnc.visit(cu, null);
+
+      MethodDeclaration md = (MethodDeclaration) cu.getTypes().get(0).getMembers().get(0);
+
+      BlockStmt block = md.getBody();
+
+      IfStmt stmt = (IfStmt) block.getStmts().get(0);
+
+      BinaryExpr be = (BinaryExpr) stmt.getCondition();
+
+      BinaryExpr lbe = (BinaryExpr) be.getLeft();
+
+      Assert.assertTrue(lbe.getOperator().equals(BinaryExpr.Operator.notEquals));
+   }
+
+   @Test
+   public void testAndWithFields() throws Exception {
+
+      CompilationUnit cu = compile(
+            "public class Foo{ private String name; public void bar(){ if(this.name.equals(\"hello\") && name != null) {}} }");
+
+      MisplacedNullCheck<?> mnc = new MisplacedNullCheck<Object>();
+
+      mnc.visit(cu, null);
+
+      MethodDeclaration md = (MethodDeclaration) cu.getTypes().get(0).getMembers().get(1);
+
+      BlockStmt block = md.getBody();
+
+      IfStmt stmt = (IfStmt) block.getStmts().get(0);
+
+      BinaryExpr be = (BinaryExpr) stmt.getCondition();
+
+      BinaryExpr lbe = (BinaryExpr) be.getLeft();
+
+      Assert.assertTrue(lbe.getOperator().equals(BinaryExpr.Operator.notEquals));
+   }
+
+   @Test
+   public void testAndWithFieldsOfDifferentObjects() throws Exception {
+
+      CompilationUnit cu = compile(
+            "public class Foo{ private String name; public void bar(Foo aux){ if(this.name.equals(\"hello\") && aux.name != null) {}} }");
+
+      MisplacedNullCheck<?> mnc = new MisplacedNullCheck<Object>();
+
+      mnc.visit(cu, null);
+
+      MethodDeclaration md = (MethodDeclaration) cu.getTypes().get(0).getMembers().get(1);
+
+      BlockStmt block = md.getBody();
+
+      IfStmt stmt = (IfStmt) block.getStmts().get(0);
+
+      BinaryExpr be = (BinaryExpr) stmt.getCondition();
+
+      Assert.assertTrue(be.getLeft() instanceof MethodCallExpr);
+   }
+
+   @Test
+   public void testOr() throws Exception {
+
+      CompilationUnit cu = compile(
+            "public class Foo{ public void bar(Object c){ if(c.toString().equals(\"hello\") || c== null) {}} }");
+
+      MisplacedNullCheck<?> mnc = new MisplacedNullCheck<Object>();
+
+      mnc.visit(cu, null);
+
+      MethodDeclaration md = (MethodDeclaration) cu.getTypes().get(0).getMembers().get(0);
+
+      BlockStmt block = md.getBody();
+
+      IfStmt stmt = (IfStmt) block.getStmts().get(0);
+
+      BinaryExpr be = (BinaryExpr) stmt.getCondition();
+
+      BinaryExpr lbe = (BinaryExpr) be.getLeft();
+
+      Assert.assertTrue(lbe.getOperator().equals(BinaryExpr.Operator.equals));
+   }
+
+   @Test
+   public void testAndWithHighCommonAncestors() throws Exception {
+
+      CompilationUnit cu = compile(
+            "public class Foo{ public void bar(Object c){ if((c.toString().equals(\"hello\") && true == true) && c!= null) {}} }");
+
+      MisplacedNullCheck<?> mnc = new MisplacedNullCheck<Object>();
+
+      mnc.visit(cu, null);
+
+      MethodDeclaration md = (MethodDeclaration) cu.getTypes().get(0).getMembers().get(0);
+
+      BlockStmt block = md.getBody();
+
+      IfStmt stmt = (IfStmt) block.getStmts().get(0);
+
+      BinaryExpr be = (BinaryExpr) stmt.getCondition();
+
+      BinaryExpr lbe = (BinaryExpr) be.getLeft();
+
+      Assert.assertTrue(lbe.getOperator().equals(BinaryExpr.Operator.notEquals));
+
+   }
+}
